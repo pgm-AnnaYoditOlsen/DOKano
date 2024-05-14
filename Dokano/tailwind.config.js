8c3d10205d41e1e17997032e74a999f73059592a
DriesD1 <Dries.dhondt1@student.arteveldehs.be>
2024-05-14 14:41:23 +0200

    added vendor map & tailwind themes

--- Dokano/tailwind.config.js
+++ Dokano/tailwind.config.js
@@ -8,8 +8,26 @@ export default {
         './content/**/*.md',
     ],
 
-    theme: {
-        extend: {},
+theme: {
+        extend: {
+            colors: {
+                theme1: {
+                    primary: '#1d4ed8', // Example primary color for theme 1
+                    secondary: '#9333ea', // Example secondary color for theme 1
+                    accent: '#f59e0b', // Example accent color for theme 1
+                },
+                theme2: {
+                    primary: '#10b981', // Example primary color for theme 2
+                    secondary: '#3b82f6', // Example secondary color for theme 2
+                    accent: '#ef4444', // Example accent color for theme 2
+
+                },
+            },
+            fontFamily: {
+                theme1: ['Roboto', 'sans-serif'], // Example font family for theme 1
+                theme2: ['Open Sans', 'sans-serif'], // Example font family for theme 2
+            },
+        },
     },
 
     plugins: [
