5c3cc667a34d20789e60d5d13f14968868fac2e5
DriesD1 <Dries.dhondt1@student.arteveldehs.be>
2024-05-21 15:31:27 +0200

    added taxonomies

---
+++ Dokano/resources/js/formuleKano.js
@@ -0,0 +1,23 @@
+// Selecteer alle categorie-elementen en kano-elementen
+const categories = document.querySelectorAll('.radio-formule-wrapper input[name="formule"]');
+const canoes = document.querySelectorAll('.radio-formule-wrapper input[name="type_kano"]');
+
+// Voeg een click event listener toe aan elk categorie-element
+categories.forEach(category => {
+  category.addEventListener('click', () => {
+    // Haal de geselecteerde waarde van de categorie op
+    const selectedCategory = category.value;
+    
+    // Verberg alle kano's
+    canoes.forEach(canoe => {
+      canoe.closest('div').classList.add('hidden');
+    });
+
+    // Toon de kano's van de geselecteerde categorie
+    const selectedCanoes = document.querySelectorAll(`[data-categories*="${selectedCategory}"]`);
+    console.log(selectedCanoes);
+    selectedCanoes.forEach(canoe => {
+      canoe.closest('div').classList.remove('hidden');
+    });
+  });
+});
--- vendor/jezzdk/statamic-google-maps/resources/js/addon.js
+++
@@ -1,5 +0,0 @@
-import Fieldtype from './components/GoogleMapFieldtype.vue';
-
-Statamic.booting(() => {
-    Statamic.$components.register('google_map-fieldtype', Fieldtype);
-});
