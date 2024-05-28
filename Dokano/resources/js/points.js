4497542e2b6c8d15d413475bd33f274531fe5c0b
DriesD1 <Dries.dhondt1@student.arteveldehs.be>
2024-05-28 19:21:37 +0200

    nav points under picture fixed

---
+++ Dokano/resources/js/points.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to observe and set active navigation points
+    const observeAndSetActivePoints = (itemSelector, navPointSelector, activeClass) => {
+      const items = document.querySelectorAll(itemSelector);
+      const navPoints = document.querySelectorAll(navPointSelector);
+  
+      const observerOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5 // Adjust this threshold as needed
+      };
+  
+      const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const index = Array.from(items).indexOf(entry.target);
+            setActivePoint(index, navPoints, activeClass);
+          }
+        });
+      };
+  
+      const observer = new IntersectionObserver(observerCallback, observerOptions);
+      items.forEach(item => observer.observe(item));
+    };
+  
+    const setActivePoint = (activeIndex, navPoints, activeClass) => {
+      navPoints.forEach((point, index) => {
+        if (index === activeIndex) {
+          point.classList.add(activeClass);
+          point.classList.remove("bg-standards-grey");
+        } else {
+          point.classList.remove(activeClass);
+          point.classList.add("bg-standards-grey");
+        }
+      });
+    };
+  
+    // Observe and set active points for event items
+    observeAndSetActivePoints("[id^='event']", ".event-nav-point", "bg-DOKano-lightblue");
+  
+    // Observe and set active points for activity items
+    observeAndSetActivePoints("[id^='activity']", ".activity-nav-point", "bg-DOKano-lightblue");
+  });
+  
