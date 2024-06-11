7465b09e454a4d368c1fdf24f9913d68db4d7b99
DriesD1 <Dries.dhondt1@student.arteveldehs.be>
2024-06-11 15:34:23 +0200

    fixed little bug

--- Dokano/resources/js/canoeChecker.js
+++ Dokano/resources/js/canoeChecker.js
@@ -2,18 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     // DOM elements initialization
     const bookingDateInput = document.getElementById("datePicker");
     const bookingTimeInput = document.getElementById("tijd");
-    const checkAvailabilityButton =
-        document.getElementById("checkAvailability");
+    const checkAvailabilityButton = document.getElementById("checkAvailability");
     const nextButton = document.getElementById("next1");
-    const canoeAvailabilitySection =
-        document.getElementById("canoeAvailability");
+    const canoeAvailabilitySection = document.getElementById("canoeAvailability");
     const aantalKanosInput = document.getElementById("aantal_kanos");
-    const categories = document.querySelectorAll(
-        '.radio-formule-wrapper input[name="formule"]'
-    );
-    const canoes = document.querySelectorAll(
-        '.radio-formule-wrapper input[name="type_kano"]'
-    );
+    const categories = document.querySelectorAll('.radio-formule-wrapper input[name="formule"]');
+    const canoes = document.querySelectorAll('.radio-formule-wrapper input[name="type_kano"]');
     const error = document.getElementById("error");
 
     let availableCanoes = {};
@@ -21,9 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Fetch available canoes for the selected date and time
     const fetchAvailableCanoes = async (date, time) => {
         try {
-            const response = await fetch(
-                `/api/check_availability?date=${date}&time=${time}`
-            );
+            const response = await fetch(`/api/check_availability?date=${date}&time=${time}`);
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
@@ -47,10 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
         }
 
-        const availability = await fetchAvailableCanoes(
-            selectedDate,
-            selectedTime
-        );
+        const availability = await fetchAvailableCanoes(selectedDate, selectedTime);
         if (availability !== null) {
             if (availability.error) {
                 canoeAvailabilitySection.innerHTML = `<p>Error: ${availability.error}</p>`;
@@ -59,149 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let availabilityHtml = "";
 
                 // Update availability information and visibility of canoes
-                for (const [canoeType, remainingCanoes] of Object.entries(
-                    availability
-                )) {
-                    availabilityHtml += `<p class="text-standards-grey mb-2">${canoeType}: ${remainingCanoes}</p>`;
-                    const canoeElement = document.querySelector(
-                        `[data-canoe-type="${canoeType}"]:not(.hidden)`
-                    );
-                    if (canoeElement) {
-                        if (remainingCanoes === 0) {
-                            canoeElement.classList.add("none");
-                        } else {
-                            canoeElement.classList.remove("none");
-                        }
-                    }
-                }
-
-                canoeAvailabilitySection.innerHTML = availabilityHtml;
-            }
-        } else {
-            canoeAvailabilitySection.innerHTML =
-                "<p>Error fetching available canoes. Please try again.</p>";
-        }
-    };
-
-    // Check canoe availability before proceeding to the next form step
-    const checkCanoeAvailabilityBeforeNext = () => {
-        const requestedCanoes = parseInt(aantalKanosInput.value, 10);
-        const selectedCanoeType = document.querySelector(
-            'input[name="type_kano"]:checked'
-        );
-
-        if (selectedCanoeType) {
-            const canoeType = selectedCanoeType.value;
-            const available = availableCanoes[canoeType] || 0;
-            if (requestedCanoes > available) {
-                alert("Teveel kanos voor geselecteerde type kano");
-                error.innerHTML = "Teveel kanos voor geselecteerde type kano";
-                return false; // Prevent form submission or moving to next step
-            }
-        } else {
-            return false; // Prevent form submission or moving to next step
-        }
-        return true;
-    };
-
-    // Event listener for the "Check Availability" button
-    checkAvailabilityButton.addEventListener("click", updateCanoeAvailability);
-
-    // Event listener for the "Next" button
-    nextButton.addEventListener("click", (event) => {
-        // Check canoe availability before moving to the next step
-        if (!checkCanoeAvailabilityBeforeNext() && !innerHTML == "") {
-            event.preventDefault(); // Prevent moving to the next step if validation fails
-            event.stopPropagation(); // Stop the event from propagating further
-        }
-    });
-
-    // Handle category selection and show/hide canoes accordingly
-    categories.forEach((category) => {
-        category.addEventListener("click", () => {
-            const selectedCategory = category.value.toLowerCase();
-
-            // Hide all canoes initially
-            canoes.forEach((canoe) => {
-                canoe.closest("div").classList.add("hidden");
-            });
-
-            // Show only the canoes related to the selected formula
-            document.querySelectorAll(`[data-categories]`).forEach((canoe) => {
-                const canoeCategories = canoe
-                    .getAttribute("data-categories")
-                    .toLowerCase();
-                if (canoeCategories.includes(selectedCategory)) {
-                    canoe.closest("div").classList.remove("hidden");
-                }
-            });
-        });
-    });
-});
-document.addEventListener("DOMContentLoaded", () => {
-    // DOM elements initialization
-    const bookingDateInput = document.getElementById("datePicker");
-    const bookingTimeInput = document.getElementById("tijd");
-    const checkAvailabilityButton =
-        document.getElementById("checkAvailability");
-    const nextButton = document.getElementById("next1");
-    const canoeAvailabilitySection =
-        document.getElementById("canoeAvailability");
-    const aantalKanosInput = document.getElementById("aantal_kanos");
-    const categories = document.querySelectorAll(
-        '.radio-formule-wrapper input[name="formule"]'
-    );
-    const canoes = document.querySelectorAll(
-        '.radio-formule-wrapper input[name="type_kano"]'
-    );
-    const error = document.getElementById("error");
-
-    let availableCanoes = {};
-
-    // Fetch available canoes for the selected date and time
-    const fetchAvailableCanoes = async (date, time) => {
-        try {
-            const response = await fetch(
-                `/api/check_availability?date=${date}&time=${time}`
-            );
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const textData = await response.text();
-            const data = JSON.parse(textData);
-            return data;
-        } catch (error) {
-            console.error("Error fetching available canoes:", error);
-            return null;
-        }
-    };
-
-    // Update the canoe availability based on the selected date and time
-    const updateCanoeAvailability = async () => {
-        const selectedDate = bookingDateInput.value;
-        const selectedTime = bookingTimeInput.value;
-
-        if (!selectedDate || !selectedTime) {
-            canoeAvailabilitySection.innerHTML =
-                '<p class="text-center font-bold text-l">Selecteer aub een datum en een tijdstip</p>';
-            return;
-        }
-
-        const availability = await fetchAvailableCanoes(
-            selectedDate,
-            selectedTime
-        );
-        if (availability !== null) {
-            if (availability.error) {
-                canoeAvailabilitySection.innerHTML = `<p>Error: ${availability.error}</p>`;
-            } else {
-                availableCanoes = availability; // Store the availability globally
-                let availabilityHtml = "";
-
-                // Update availability information and visibility of canoes
-                for (const [canoeType, remainingCanoes] of Object.entries(
-                    availability
-                )) {
+                for (const [canoeType, remainingCanoes] of Object.entries(availability)) {
                     availabilityHtml += `
                     <p class="canoe-info text-standards-grey mt-2 mb-2">
                         <span class="canoe-type">${canoeType}: </span> 
@@ -231,31 +78,72 @@ document.addEventListener("DOMContentLoaded", () => {
     // Check canoe availability before proceeding to the next form step
     const checkCanoeAvailabilityBeforeNext = () => {
         const requestedCanoes = parseInt(aantalKanosInput.value, 10);
-        const selectedCanoeType = document.querySelector(
-            'input[name="type_kano"]:checked'
-        );
+        const selectedCanoeType = document.querySelector('input[name="type_kano"]:checked');
 
         if (selectedCanoeType) {
             const canoeType = selectedCanoeType.value;
             const available = availableCanoes[canoeType] || 0;
             if (requestedCanoes > available) {
-                alert("Teveel kanos voor geselecteerde type kano");
                 error.innerHTML = "Teveel kanos voor geselecteerde type kano";
                 return false; // Prevent form submission or moving to next step
             }
         } else {
+            error.innerHTML = "Selecteer alstublieft een kano.";
             return false; // Prevent form submission or moving to next step
         }
         return true;
     };
+
+    // Function to validate the form
+    const validateForm = () => {
+        const selectedFormulaInput = document.querySelector('[name="formule"]:checked');
+        const date = document.getElementById("datePicker").value;
+        const time = document.getElementById("tijd").value;
+        const adults = document.getElementById("aantal_volwassenen").value;
+        const children = document.getElementById("aantal_kinderen").value;
+        const amountCanoe = document.getElementById("aantal_kanos").value;
+        const selectedCanoeInput = document.querySelector(
+            '[name="type_kano"]:checked'
+        );
+        const canoe = selectedCanoeInput ? selectedCanoeInput.value : "";
+        const comments = document.getElementById("opmerkingen").value;
+        const error = document.getElementById("error");
+
+        // Reset error message
+        error.innerHTML = "";
+
+        if (!selectedFormulaInput) {
+            error.innerHTML += "Selecteer alstublieft een formule.<br>";
+        }
+        if (date === "") {
+            error.innerHTML += "Selecteer alstublieft een datum.<br>";
+        }
+        if (time === "Kies uw tijd") {
+            error.innerHTML += "Selecteer alstublieft een tijd.<br>";
+        }
+        if ((adults === "0" && children === "0") || (adults === "" && children === "")) {
+            error.innerHTML += "Voer alstublieft het aantal volwassenen of kinderen in.<br>";
+        }
+        if (canoe === "") {
+            error.innerHTML += "Selecteer alstublieft een kano.<br>";
+        }
+        if (comments === "") {
+            error.innerHTML += "Voer alstublieft een opmerking in.<br>";
+        }
+        if (amountCanoe === "0" || amountCanoe === "") {
+            error.innerHTML += "Voer alstublieft het aantal kano's in.<br>";
+        }
+
+        return error.innerHTML === "";
+    }
 
     // Event listener for the "Check Availability" button
     checkAvailabilityButton.addEventListener("click", updateCanoeAvailability);
 
     // Event listener for the "Next" button
     nextButton.addEventListener("click", (event) => {
-        // Check canoe availability before moving to the next step
-        if (!checkCanoeAvailabilityBeforeNext()) {
+        // Check form validation and canoe availability before moving to the next step
+        if (!validateForm() || !checkCanoeAvailabilityBeforeNext()) {
             event.preventDefault(); // Prevent moving to the next step if validation fails
             event.stopPropagation(); // Stop the event from propagating further
         }
@@ -273,9 +161,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             // Show only the canoes related to the selected formula
             document.querySelectorAll(`[data-categories]`).forEach((canoe) => {
-                const canoeCategories = canoe
-                    .getAttribute("data-categories")
-                    .toLowerCase();
+                const canoeCategories = canoe.getAttribute("data-categories").toLowerCase();
                 if (canoeCategories.includes(selectedCategory)) {
                     canoe.closest("div").classList.remove("hidden");
                 }
